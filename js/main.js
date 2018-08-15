7bd3b9f1068b244aa8f6a537825999ec28b7c493
KumarMitra <kaushikmitra556@gmail.com>
2018-08-15 10:23:00 +0530

    Bug Fix

--- js/main.js
+++ js/main.js
@@ -1,33 +1,31 @@
-var flag = 0;
-var count = 0;
+const bot = new RiveScript();
+bot.loadFile("brain.rive")
+	.then(() => {
+		console.log("Loading done!!");
+		bot.sortReplies();
+		document.querySelector("#submit").addEventListener("click", function () {
+			let input = document.querySelector("#input").value;
 
-var bot = new RiveScript();
-bot.loadFile("brain.rive", loading_done, loading_error);
+			if (!input) return;
 
-function loading_done () {
-	console.log("Loading done!!");
-	bot.sortReplies();
-	flag = 1;
-}
+			bot.reply("local-user", input)
+				.then((reply) => {
+					let output = `
+					<li>
+						<span class='user'>You</span>
+						<p class='message'>${input}</p>
+					</li>
+					<li>
+						<span class='user'>Bot</span>
+						<p class='message'>${reply}</p>
+					</li>`;
 
-
-function loading_error () {
-	console.log("error occured");
-}
-
-document.querySelector("#submit").addEventListener("click", function(){
-	var input = document.querySelector("#input").value;
-
-	if(!input || flag == 0) return;
-
-	var reply = bot.reply("local-user", input);
-	console.log("Bot: " + reply);
-
-	var output = "<li><span class='user'>You</span><p class='message'>"+input+"</p></li>";
-	output += "<li><span class='user'>Bot</span><p class='message'>"+reply+"</p></li>";
-
-	if(count++ == 0)
-		document.querySelector("#output").innerHTML = " ";
-	document.querySelector("#output").innerHTML += output;
-});
-
+					if (document.querySelector("#output div"))
+						document.querySelector("#output").innerHTML = " ";
+					document.querySelector("#output").innerHTML += output;
+				})
+		});
+	})
+	.catch((e) => {
+		console.log(`Error: ${e}`);
+	});
